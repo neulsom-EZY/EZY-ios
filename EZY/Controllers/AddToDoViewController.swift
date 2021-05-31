996b53af3f08a3c30d17c69665a7abdf3ed81d24
Jihoon <68891494+JiHoonAHN@users.noreply.github.com>
2021-05-31 19:47:47 +0900

    [CREATE] extensions

---
+++ EZY/Controllers/AddToDoViewController.swift
@@ -0,0 +1,63 @@
+//
+//  AddToDoViewController.swift
+//  EZY
+//
+//  Created by Ji-hoon Ahn on 2021/05/31.
+//
+
+import UIKit
+import SnapKit
+import Then
+import Alamofire
+
+class AddToDoViewController:UIViewController{
+    
+    //MARK: - Properties
+    private let backbutton : UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .EZY_BAC8FF
+        button.setImage(UIImage(systemName: "arrow.left"), for: .normal)
+        return button
+    }()
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    //MARK: - Selectors
+    
+    
+    
+    //MARK: - Helpers
+    func configureUI(){
+        view.addSubview(backbutton)
+    }
+}
+
+#if DEBUG
+import SwiftUI
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    
+func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+}
+@available(iOS 13.0.0, *)
+func makeUIViewController(context: Context) -> UIViewController{
+    AddToDoViewController()
+    }
+}
+@available(iOS 13.0, *)
+struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ViewControllerRepresentable()
+                .ignoresSafeArea()
+                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        }
+        
+    }
+} #endif
---
+++ EZY/Utils/extensions.swift
@@ -0,0 +1,41 @@
+//
+//  extensions.swift
+//  EZY
+//
+//  Created by Ji-hoon Ahn on 2021/05/31.
+//
+
+import UIKit
+
+extension UIColor{
+    static func rgb(red: CGFloat ,green: CGFloat,blue:CGFloat) -> UIColor{
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    static let EZY_BAC8FF = UIColor.rgb(red: 186, green: 200, blue: 255)
+    static let EZY_CFCBFF = UIColor.rgb(red: 207, green: 203, blue: 255)
+    static let EZY_AFADFF = UIColor.rgb(red: 175, green: 173, blue: 255)
+    static let EZY_8099FF = UIColor.rgb(red: 128, green: 153, blue: 255)
+    static let EZY_F4F6FF = UIColor.rgb(red: 244, green: 246, blue: 255)
+    static let EZY_968DFF = UIColor.rgb(red: 150, green: 141, blue: 255)
+    static let EZY_939393 = UIColor.rgb(red: 147, green: 147, blue: 147)
+    static let EZY_FFCDB8 = UIColor.rgb(red: 255, green: 205, blue: 184)
+    static let EZY_BADEFF = UIColor.rgb(red: 186, green: 222, blue: 255)
+    static let EZY_CFE3CE = UIColor.rgb(red: 207, green: 227, blue: 255)
+    static let EZY_E4C9FF = UIColor.rgb(red: 228, green: 201, blue: 255)
+    static let EZY_6B40FF = UIColor.rgb(red: 107, green: 64, blue: 255)
+    static let EZY_FFA680 = UIColor.rgb(red: 255, green: 166, blue: 128)
+    static let EZY_FFD18D = UIColor.rgb(red: 255, green: 209, blue: 141)
+    static let EZY_B880FF = UIColor.rgb(red: 184, green: 128, blue: 255)
+    static let EZY_F6F3FF = UIColor.rgb(red: 246, green: 243, blue: 255)
+    static let EZY_C7E0D4 = UIColor.rgb(red: 199, green: 224, blue: 212)
+    static let EZY_FFCBB5 = UIColor.rgb(red: 255, green: 203, blue: 181)
+    static let EZY_FFB5B5 = UIColor.rgb(red: 255, green: 181, blue: 181)
+    static let EZY_ADCAE5 = UIColor.rgb(red: 173, green: 202, blue: 229)
+    static let EZY_EED9B9 = UIColor.rgb(red: 238, green: 217, blue: 185)
+    static let EZY_FFCEB9 = UIColor.rgb(red: 255, green: 206, blue: 185)
+    static let EZY_DEC4FF = UIColor.rgb(red: 222, green: 196, blue: 255)
+
+    
+    // static let EZY_ = UIColor.rgb(red: , green: , blue: )
+    
+}
