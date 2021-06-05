60ec6f3cb17136bee3a7d71d4e3ca3fdb3867eb5
YeonJu <yjnoh1016@naver.com>
2021-06-05 12:14:46 +0900

    [UPDATE] Extension Updated

--- EZY/Utils/Extension.swift
+++ EZY/Utils/Extension.swift
@@ -36,3 +36,34 @@ extension UIColor{
     static let EZY_FFCEB9 = UIColor.rgb(red: 255, green: 206, blue: 185)
     static let EZY_DEC4FF = UIColor.rgb(red: 222, green: 196, blue: 255)
 }
+
+extension UILabel {
+    func dynamicFont(fontSize size: CGFloat, currentFontName: String) {
+    let bounds = UIScreen.main.bounds
+    let height = bounds.size.height
+    
+    switch height {
+    case 480.0: //Iphone 3,4S => 3.5 inch
+        self.font = UIFont(name: currentFontName, size: size * 0.7)
+      break
+    case 568.0: //iphone 5, SE => 4 inch
+        self.font = UIFont(name: currentFontName, size: size * 0.8)
+      break
+    case 667.0: //iphone 6, 6s, 7, 8 => 4.7 inch
+        self.font = UIFont(name: currentFontName, size: size * 0.92)
+      break
+    case 736.0: //iphone 6s+ 6+, 7+, 8+ => 5.5 inch
+        self.font = UIFont(name: currentFontName, size: size * 0.95)
+      break
+    case 812.0: //iphone X, XS => 5.8 inch
+        self.font = UIFont(name: currentFontName, size: size)
+      break
+    case 896.0: //iphone XR => 6.1 inch  // iphone XS MAX => 6.5 inch
+        self.font = UIFont(name: currentFontName, size: size * 1.15)
+      break
+    default:
+      print("not an iPhone")
+      break
+    }
+  }
+}
