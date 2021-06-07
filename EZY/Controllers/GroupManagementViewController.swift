ce54b0f9b41f439fb98ecebf97f4002c9eceee28
Y00ujin <yujingim43@gmail.com>
2021-06-07 14:32:42 +0900

    [CREATE] GroupManagementViewController.swift created

---
+++ EZY/Controllers/GroupManagementViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GroupManagementViewController.swift
+//  EZY
+//
+//  Created by 김유진 on 2021/06/07.
+//
+
+import UIKit
+
+class GroupManagementViewController: UIViewController {
+
+    //MARK: Properties
+    
+    
+    //MARK: LifeCycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
