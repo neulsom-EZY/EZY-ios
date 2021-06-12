b833531447edc794c66922215a71a900570d6e49
YeonJu <yjnoh1016@naver.com>
2021-06-12 23:50:56 +0900

    [UPDATE] SignUpNicknameViewController Complete

--- EZY/Controllers/SignUpNicknameViewController.swift
+++ EZY/Controllers/SignUpNicknameViewController.swift
@@ -9,3 +9,132 @@ import UIKit
 import SnapKit
 import Then
 
+class SignUpNicknameViewController: UIViewController{
+    //MARK: - Properties
+    
+    lazy var goBackButton = UIButton().then {
+        $0.setImage(UIImage(named: "EZY_GoBackBtn"), for: .normal)
+    }
+    
+    lazy var EZY_Logo = UIImageView().then {
+        $0.image = UIImage(named: "EZY_LOGOTYPE_2")
+    }
+    
+    lazy var putNicknameLabel = UILabel().then {
+        $0.text = "닉네임을\n입력해주세요."
+        $0.numberOfLines = 2
+        $0.dynamicFont(fontSize: 25, currentFontName: "AppleSDGothicNeo-SemiBold")
+        $0.textColor = UIColor.EZY_968DFF
+    }
+    
+    lazy var nicknameContainerView: UIView = {
+        let view = Utilities().inputContainerView(textField: nicknameField, text: "닉네임", fonts: 14)
+        return view
+    }()
+    
+    lazy var nicknameField:UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "")
+        return tf
+    }()
+    
+    lazy var alreadyExistLabel = UILabel().then {
+        $0.text = "이미 존재하는 닉네임입니다!"
+        $0.textColor = UIColor.EZY_FCA1A1
+        $0.dynamicFont(fontSize: 10, currentFontName: "AppleSDGothicNeo-SemiBold")
+    }
+    
+    lazy var continueButton = UIButton().then {
+        $0.setTitle("계속하기", for: .normal)
+        $0.titleLabel?.dynamicFont(fontSize: 14, currentFontName: "AppleSDGothicNeo-Bold")
+        $0.setTitleColor(UIColor.EZY_FFFFFF, for: UIControl.State.normal)
+        $0.backgroundColor = UIColor.EZY_968DFF
+    }
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    //MARK: - Selectors
+
+    //MARK: - Helpers
+    func configureUI(){
+        view.backgroundColor = .white
+        view.addSubview(goBackButton)
+        view.addSubview(EZY_Logo)
+        view.addSubview(putNicknameLabel)
+        view.addSubview(nicknameContainerView)
+        view.addSubview(alreadyExistLabel)
+        view.addSubview(continueButton)
+        
+        continueButton.layer.cornerRadius = self.view.frame.width/31.5
+        
+        goBackButton.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(self.view.frame.height/9.12)
+            make.left.equalToSuperview().offset(self.view.frame.width/9.14)
+            make.height.equalTo(self.view.frame.height/50.75)
+            make.width.equalTo(self.view.frame.width/41.67)
+        }
+        
+        EZY_Logo.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(self.view.frame.height/10.55)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(self.view.frame.height/20.3)
+            make.width.equalTo(self.view.frame.width/4.46)
+        }
+        
+        putNicknameLabel.snp.makeConstraints { make in
+            make.top.equalTo(EZY_Logo).offset(self.view.frame.height/9.67)
+            make.left.equalToSuperview().offset(self.view.frame.width/7.5)
+        }
+        
+        nicknameContainerView.snp.makeConstraints { make in
+            make.top.equalTo(putNicknameLabel).offset(self.view.frame.height/8.29)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(self.view.frame.width/1.34)
+            make.height.equalTo(self.view.frame.height/15.62)
+        }
+        
+        alreadyExistLabel.snp.makeConstraints { make in
+            make.top.equalTo(nicknameContainerView).offset(self.view.frame.height/13.76)
+            make.left.equalToSuperview().offset(self.view.frame.width/7.5)
+        }
+        
+        continueButton.snp.makeConstraints { make in
+            make.top.equalTo(alreadyExistLabel).offset(self.view.frame.height/6.34)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(self.view.frame.width/1.13)
+            make.height.equalTo(self.view.frame.height/16.24)
+        }
+    }
+}
+
+
+//MARK: - Preview
+#if DEBUG
+import SwiftUI
+struct SignUpNicknameViewControllerRepresentable: UIViewControllerRepresentable {
+    
+func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+}
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController{
+        SignUpNicknameViewController()
+    }
+}
+@available(iOS 13.0, *)
+struct SignUpNicknameViewControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            SignUpNicknameViewControllerRepresentable()
+                .ignoresSafeArea()
+                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        }
+        
+    }
+} #endif
+
+
