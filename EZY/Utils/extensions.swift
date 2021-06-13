844f152f30a11439fc4ba17d13ca4118d47f89a0
Jihoon <68891494+JiHoonAHN@users.noreply.github.com>
2021-06-13 17:41:29 +0900

    [CREATE] Extension MorePeopleTodo & make CollectionView frame

--- EZY/Utils/extensions.swift
+++ EZY/Utils/extensions.swift
@@ -235,3 +235,30 @@ extension AddToDoViewController: UICollectionViewDelegate,UICollectionViewDataSo
                       height: view.frame.size.height/25.3)
     }
 }
+
+//MARK: - MorePeopleToDo
+extension MorePeopleToDo: UICollectionViewDelegate,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .white
+        cell.layer.cornerRadius = view.frame.height / 40.6
+        cell.layer.shadowColor = UIColor.gray.cgColor
+        cell.layer.shadowRadius = view.frame.height/40.6
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.size.height/9.6,
+                      height: view.frame.size.height/25.3)
+    }
+    
+    
+}
+class CustomCell : UICollectionViewCell{
+
+    
+    
+}
