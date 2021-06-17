54e5889553c8501290a3d3c331b5b94304fc98a4
Y00ujin <yujingim43@gmail.com>
2021-06-17 20:39:57 +0900

    [CREATE] PlanRequestRespondingViewController created

---
+++ EZY/Controllers/PlanRequestRespondingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PlanRequestRespondingViewController.swift
+//  EZY
+//
+//  Created by 김유진 on 2021/06/17.
+//
+
+import UIKit
+
+class PlanRequestRespondingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+    
+    
+}
