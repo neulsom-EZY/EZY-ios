89aa2f4e72b659b0650933cf6dfa0c19466b453d
Y00ujin <yujingim43@gmail.com>
2021-06-19 18:25:07 +0900

    [CREATE] planShowView created

--- EZY/Controllers/PlanRequestRespondingViewController.swift
+++ EZY/Controllers/PlanRequestRespondingViewController.swift
@@ -32,9 +32,82 @@ class PlanRequestRespondingViewController: UIViewController {
         $0.dynamicFont(fontSize: 20, currentFontName: "AppleSDGothicNeo-SemiBold")
         $0.updateGradientTextColor_vertical(gradientColors: labelColor)
     }
+    
+    lazy var planShowView = UIView().then {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 20
+        $0.layer.masksToBounds = false
+        $0.layer.shadowOpacity = 0.1
+        $0.layer.shadowRadius = 6
+        $0.layer.shadowOffset = CGSize(width: 0, height: 3)
+        $0.layer.shadowColor = UIColor.black.cgColor
+    }
+    
+    lazy var planTitleLabel = UILabel().then {
+        $0.text = "EZY 회의"
+        $0.dynamicFont(fontSize: 22, currentFontName: "AppleSDGothicNeo-Bold")
+        $0.textColor = UIColor(red: 141/255, green: 138/255, blue: 255/255, alpha: 1)
+    }
+    
+    lazy var planCalenderLabel = UILabel().then {
+        $0.text = "2021. 05. 09 화요일"
+        $0.textAlignment = .center
+        $0.textColor = UIColor(red: 101/255, green: 101/255, blue: 101/255, alpha: 1)
+        $0.dynamicFont(fontSize: 14, currentFontName: "Poppins-Regular")
+    }
+    
+    lazy var planTimeImageView = UIImageView().then {
+        $0.image = UIImage(named: "EZY_TimeSquare")
+    }
+    
+    lazy var planTimeLabel = UILabel().then {
+        $0.text = "11:00AM - 1:00PM"
+        $0.textAlignment = .center
+        $0.textColor = UIColor(red: 101/255, green: 101/255, blue: 101/255, alpha: 1)
+        $0.dynamicFont(fontSize: 14, currentFontName: "Poppins-Regular")
+    }
+    
+    lazy var planLocationImageView = UIImageView().then {
+        $0.image = UIImage(named: "EZY_Location")
+    }
+    
+    lazy var planLocationLabel = UILabel().then {
+        $0.text = "광주소프트웨어마이스터고등학교"
+        $0.textAlignment = .center
+        $0.textColor = UIColor(red: 101/255, green: 101/255, blue: 101/255, alpha: 1)
+        $0.dynamicFont(fontSize: 14, currentFontName: "Poppins-Regular")
+    }
+    
+    lazy var planExplanationView = UIView().then {
+        $0.backgroundColor = UIColor(red: 248/255, green: 248/255, blue: 248/255, alpha: 1)
+        $0.layer.cornerRadius = 10
+    }
+    
+    lazy var planCalenderImageView = UIImageView().then{
+          $0.image = UIImage(named: "EZY_Calendar.svg")
+    }
+    
+    lazy var planExplanationLabel = UILabel().then{
+        $0.text = "회의에 꼭 참가해주세요."
+        $0.textColor = UIColor(red: 101/255, green: 101/255, blue: 101/255, alpha: 1)
+        $0.textAlignment = .center
+        $0.dynamicFont(fontSize: 12, currentFontName: "AppleSDGothicNeo-Regular")
+    }
+    
+    // 버튼 글씨가 안나옴 ㅅㅂ
+    lazy var rejectionButton = UIButton().then{
+        $0.setTitle("Test Button", for: .normal)
+        $0.backgroundColor = .white
+//        $0.titleLabel?.textColor = UIColor(red: 207/255, green: 203/255, blue: 255/255, alpha: 1)
+        $0.layer.borderWidth = 1.5
+        $0.layer.borderColor = UIColor(red: 207/255, green: 203/255, blue: 255/255, alpha: 1).cgColor
+//        $0.dynamicFont(fontSize: 14, currentFontName: "AppleSDGothicNeo-Bold")
+    }
+       
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.view.backgroundColor = .white
         
         layoutSetting()
@@ -44,10 +117,21 @@ class PlanRequestRespondingViewController: UIViewController {
         self.view.addSubview(backButton)
         self.view.addSubview(titleLabel)
         self.view.addSubview(contentLabel)
-        
+        self.view.addSubview(planShowView)
+        self.view.addSubview(rejectionButton)
+        planShowView.addSubview(planTitleLabel)
+        planShowView.addSubview(planTimeImageView)
+        planShowView.addSubview(planLocationImageView)
+        planShowView.addSubview(planCalenderLabel)
+        planShowView.addSubview(planTimeLabel)
+        planShowView.addSubview(planLocationLabel)
+        planShowView.addSubview(planExplanationView)
+        planShowView.addSubview(planCalenderImageView)
+        planExplanationView.addSubview(planExplanationLabel)
+
         backButton.snp.makeConstraints { make in
             make.top.equalTo(self.view.safeAreaLayoutGuide).offset(self.view.frame.height/47.7)
-            make.left.equalToSuperview().offset(20)
+            make.left.equalToSuperview().offset(self.view.frame.width/12)
             make.width.equalToSuperview().dividedBy(33.8/2)
             make.height.equalTo(backButton.snp.width)
         }
@@ -61,8 +145,74 @@ class PlanRequestRespondingViewController: UIViewController {
             make.left.equalTo(backButton)
             make.top.equalTo(titleLabel.snp.bottom)
         }
+        
+        planShowView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.height.equalToSuperview().dividedBy(2)
+            make.width.equalToSuperview().dividedBy(1.2)
+            make.top.equalTo(contentLabel.snp.bottom).offset(self.view.frame.height/14.2)
+        }
+        
+        planTitleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(self.view.frame.height/18)
+        }
+        
+        planCalenderImageView.snp.makeConstraints { make in
+            make.top.equalTo(planTitleLabel.snp.bottom).offset(self.view.frame.height/29)
+            make.width.equalToSuperview().dividedBy(17.3)
+            make.height.equalTo(planCalenderImageView.snp.width)
+            make.centerX.equalToSuperview()
+        }
+        
+        planCalenderLabel.snp.makeConstraints { make in
+            make.top.equalTo(planCalenderImageView.snp.bottom).offset(self.view.frame.height/81.2)
+            make.centerX.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+        
+        planTimeImageView.snp.makeConstraints { make in
+            make.top.equalTo(planCalenderLabel.snp.bottom).offset(self.view.frame.height/29)
+            make.width.height.equalTo(planCalenderImageView).multipliedBy(1.2)
+            make.centerX.equalToSuperview()
+        }
+        
+        planTimeLabel.snp.makeConstraints { make in
+            make.top.equalTo(planTimeImageView.snp.bottom).offset(self.view.frame.height/81.2)
+            make.centerX.equalToSuperview()
+        }
+        
+        planLocationImageView.snp.makeConstraints { make in
+            make.top.equalTo(planTimeLabel.snp.bottom).offset(self.view.frame.height/29)
+            make.width.height.equalTo(planTimeImageView)
+            make.centerX.equalToSuperview()
+        }
+        
+        planLocationLabel.snp.makeConstraints { make in
+            make.top.equalTo(planLocationImageView.snp.bottom).offset(self.view.frame.height/81.2)
+            make.centerX.equalToSuperview()
+        }
+        
+        planExplanationView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.left.equalToSuperview().offset(self.view.frame.height/45)
+            make.top.equalTo(planLocationLabel.snp.bottom).offset(self.view.frame.height/45)
+            make.bottom.equalToSuperview().offset(-self.view.frame.height/45)
+        }
+        
+        planExplanationLabel.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
+        
+        rejectionButton.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(self.view.frame.width/5.2)
+            make.width.equalToSuperview().dividedBy(3.75)
+            make.height.equalToSuperview().dividedBy(20.3)
+            make.top.equalTo(planShowView.snp.bottom).offset(self.view.frame.height/29)
+            
+            rejectionButton.layer.cornerRadius = (self.view.frame.height/20.3)/2
+        }
     }
-    
 }
 
 extension UILabel{
