c7d91efa2e4aac65ec7ef59cf034670310fcbc32
Jihoon <68891494+JiHoonAHN@users.noreply.github.com>
2021-06-28 08:41:13 +0900

    [DELETE] ListMyToDo

--- EZY/Controllers/ConfirmCompletionAlertViewController .swift
+++ EZY/Controllers/ConfirmCompletionAlertViewController .swift
@@ -6,3 +6,12 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+class ConfirmCompletionAlertViewController : UIViewController {
+    private let backgroundView = UIView().then{
+        $0.backgroundColor = .black
+        $0.alpha = 0
+    }
+    
+}
---
+++ EZY/Controllers/ListMyToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ListMyToDo.swift
+//  EZY
+//
+//  Created by Ji-hoon Ahn on 2021/06/28.
+//
+
+import UIKit
+import SnapKit
+import Then
+import Alamofire
+
+class ListMyToDo : UIViewController {
+    
+}
