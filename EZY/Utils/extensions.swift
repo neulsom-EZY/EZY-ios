10d3f41f2706c3a256821131cf3f496293bf5ca8
Jihoon <68891494+JiHoonAHN@users.noreply.github.com>
2021-06-29 12:56:29 +0900

    [CREATE] CALayer

--- EZY/Utils/extensions.swift
+++ EZY/Utils/extensions.swift
@@ -221,7 +221,23 @@ extension UITextView {
           self.font = UIFont.systemFont(ofSize: calculatedFont!.pointSize, weight: weight)
     }
 }
-
+extension CALayer {
+func applySketchShadow(color: UIColor = .black,alpha: Float = 0.5,x:CGFloat,y:CGFloat,blur: CGFloat,spread: CGFloat = 0){
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur/UIScreen.main.scale
+        masksToBounds = false
+    
+        if spread == 0 {
+            shadowPath = nil
+        }else {
+            let dx = -spread
+            let rect = bounds.insetBy(dx: dx, dy: dx)
+            shadowPath = UIBezierPath(rect: rect).cgPath
+        }
+    }
+}
 
 
 // MARK: - AddToDoViewController 재정의
