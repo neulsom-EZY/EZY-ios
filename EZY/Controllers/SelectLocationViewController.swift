af59c86de3b4fc3a2adc733a53d745aae3d88bae
Y00ujin <yujingim43@gmail.com>
2021-07-08 10:31:06 +0900

    [CREATE] locationHalfModalView created

--- EZY/Controllers/SelectLocationViewController.swift
+++ EZY/Controllers/SelectLocationViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class SelectLocationViewController: UIViewController {
-    
+        
     lazy var backButton = UIButton().then {
         $0.setImage(UIImage(named: "EZY_LocationBackButton"), for: .normal)
     }
@@ -22,6 +22,20 @@ class SelectLocationViewController: UIViewController {
         $0.placeholder = "광주소프트웨어마이스터고등학교"
         $0.dynamicFont(fontSize: 10, currentFontName: "AppleSDGothicNeo-Bold")
     }
+    
+    lazy var locationHalfModalView = UIView().then{
+        $0.backgroundColor = .white
+        $0.roundCorners(cornerRadius: 50, maskedCorners: [.layerMinXMinYCorner, .layerMaxXMinYCorner]) // 그림자 코드 뒤에 넣으면 그림자가 안먹음
+        $0.layer.masksToBounds = false
+        $0.layer.shadowOpacity = 0.3
+        $0.layer.shadowRadius = 20
+        $0.layer.shadowOffset = CGSize(width: 0, height: 0)
+        $0.layer.shadowColor = UIColor(red: 163/255, green: 163/255, blue: 163/255, alpha: 1).cgColor
+    }
+    
+    lazy var locationTableView = UITableView().then {
+        $0.backgroundColor = .yellow
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +48,7 @@ class SelectLocationViewController: UIViewController {
     func layoutSetting(){
         self.view.addSubview(backButton)
         self.view.addSubview(textFieldBackgroundView)
+        self.view.addSubview(locationHalfModalView)
         textFieldBackgroundView.addSubview(locationTextField)
         
         backButton.snp.makeConstraints { make in
@@ -49,10 +64,16 @@ class SelectLocationViewController: UIViewController {
             make.height.equalToSuperview().dividedBy(21.3)
             make.width.equalToSuperview().dividedBy(1.5)
         }
+        
         locationTextField.snp.makeConstraints { make in
-            make.width.equalToSuperview().dividedBy(1.09)
+            make.width.equalToSuperview().dividedBy(1.19)
             make.centerX.equalToSuperview()
             make.height.equalToSuperview()
+        }
+        
+        locationHalfModalView.snp.makeConstraints { make in
+            make.bottom.left.right.equalToSuperview()
+            make.height.equalToSuperview().dividedBy(4)
         }
     }
 
