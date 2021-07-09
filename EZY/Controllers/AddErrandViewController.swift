19cfd5a94115ebdf4adb75a5e5c3a51d17d7927b
Jihoon <68891494+JiHoonAHN@users.noreply.github.com>
2021-07-10 00:51:41 +0900

    [CREATE] UserView file

--- EZY/Controllers/AddErrandViewController.swift
+++ EZY/Controllers/AddErrandViewController.swift
@@ -228,3 +228,28 @@ class AddErrandViewController : UIViewController{
 }
 
 
+
+#if DEBUG
+import SwiftUI
+struct LoginViewControllerRepresentable: UIViewControllerRepresentable {
+    
+func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+}
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController{
+        AddErrandViewController()
+    }
+}
+@available(iOS 13.0, *)
+struct LoginViewControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LoginViewControllerRepresentable()
+                .ignoresSafeArea()
+                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        }
+        
+    }
+} #endif
---
+++ EZY/Views/UserView.swift
@@ -0,0 +1,14 @@
+//
+//  UserView.swift
+//  EZY
+//
+//  Created by Ji-hoon Ahn on 2021/07/10.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class UserView: UIView{
+    
+}
