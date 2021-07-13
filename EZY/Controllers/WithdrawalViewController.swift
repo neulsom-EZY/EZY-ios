3de213eb34a2bf8d0ddbfe8a61d26ca9b2303d05
Y00ujin <yujingim43@gmail.com>
2021-07-13 18:11:50 +0900

    [CREATE] exit

--- EZY/Controllers/WithdrawalViewController.swift
+++ EZY/Controllers/WithdrawalViewController.swift
@@ -30,20 +30,27 @@ class WithdrawalViewController: UIViewController {
     
     lazy var pwNameLabel = UILabel().then {
         $0.textColor = UIColor(red: 150/255, green: 141/255, blue: 255/255, alpha: 1)
-        $0.text = "아이디/닉네임"
+        $0.text = "비밀번호"
         $0.dynamicFont(fontSize: 10, currentFontName: "AppleSDGothicNeo-SemiBold")
     }
     
     lazy var pwTextField = UITextField().then {
         $0.textColor = UIColor(red: 101/255, green: 101/255, blue: 101/255, alpha: 1)
-        $0.placeholder = "아이디를 입력해주세요"
+        $0.placeholder = "비밀번호를 입력해주세요"
         $0.dynamicFont(fontSize: 14, currentFontName: "AppleSDGothicNeo-Regular")
     }
     
     lazy var pwUnderLineView = UIView().then {
         $0.backgroundColor = UIColor(red: 150/255, green: 141/255, blue: 255/255, alpha: 1)
     }
-
+    
+    lazy var withdrawalButton = UIButton().then {
+        $0.setBackgroundImage(UIImage(named: "EZY_IdChangeButtonImage"), for: .normal)
+        $0.setTitle("변경하기", for: .normal)
+        $0.alpha = 0.5
+        $0.dynamicFont(fontSize: 14, currentFontName: "AppleSDGothicNeo-Bold")
+        $0.addTarget(self, action: #selector(withdrawalButtonClicked(sender:)), for: .touchUpInside)
+    }
 
     //MARK: - LifeCycle
     override func viewDidLoad() {
@@ -54,6 +61,10 @@ class WithdrawalViewController: UIViewController {
         layoutSetting()
     }
     
+    @objc func withdrawalButtonClicked(sender:UIButton){
+        self.navigationController?.popViewController(animated: true)
+    }
+    
     func layoutSetting(){
         self.view.backgroundColor = .white
         self.view.addSubview(idNameLabel)
@@ -62,6 +73,7 @@ class WithdrawalViewController: UIViewController {
         self.view.addSubview(pwNameLabel)
         self.view.addSubview(pwTextField)
         self.view.addSubview(pwUnderLineView)
+        self.view.addSubview(withdrawalButton)
         
         idNameLabel.snp.makeConstraints { make in
             make.top.equalTo(topView.titleLabel.snp.bottom).offset(self.view.frame.height/16.91)
@@ -101,6 +113,12 @@ class WithdrawalViewController: UIViewController {
             make.height.equalTo(2)
         }
         
+        withdrawalButton.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(self.view.frame.width/17)
+            make.centerX.equalToSuperview()
+            make.height.equalToSuperview().dividedBy(16.24)
+            make.bottom.equalToSuperview().offset(-self.view.frame.height/23.8)
+        }
     }
     
     func topViewSetting(){
