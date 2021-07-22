c39d248c9424d5e16b279d978e018e59f00cc2b8
Y00ujin <yujingim43@gmail.com>
2021-07-22 16:18:39 +0900

    [CREATE] tagTableView created

--- EZY/Controllers/TagManagementViewController.swift
+++ EZY/Controllers/TagManagementViewController.swift
@@ -10,23 +10,48 @@ import UIKit
 class TagManagementViewController: UIViewController {
     
     lazy var backButton = UIButton().then{
-        $0.setImage(UIImage(named: "EZY_IdChangeBackButtonImage"), for: .normal)
+        $0.setImage(UIImage(named: "EZY_TagManagementBackButtonImage"), for: .normal)
     }
     
     lazy var mainTitleLabel = UILabel().then {
         $0.text = "태그 관리"
-        $0.textColor = UIColor(red: 107/255, green: 64/255, blue: 255/255, alpha: 1)
+        $0.textColor = UIColor(red: 150/255, green: 141/255, blue: 255/255, alpha: 1)
         $0.dynamicFont(fontSize: 22, currentFontName: "Poppins-SemiBold")
+    }
+    
+    lazy var tagAddButton = UIButton().then{
+        $0.setImage(UIImage(named: "EZY_PlanAddButton"), for: .normal)
+    }
+
+    
+    lazy var tagTableView = UITableView().then {
+        $0.separatorStyle = .none
+        $0.showsVerticalScrollIndicator = false
+        
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         layoutSetting()
+        
+        tagTableViewSetting()
     }
-    
-    @objc func backButtonClicked(sender:UIButton){
-        self.navigationController?.popViewController(animated: true)
+
+    func tagTableViewSetting(){
+        tagTableView.delegate = self
+        tagTableView.dataSource = self
+                
+        self.view.addSubview(tagTableView)
+        
+        tagTableView.register(TagTableViewCell.self, forCellReuseIdentifier: TagTableViewCell.reuseId)
+        
+        tagTableView.snp.makeConstraints { make in
+            make.top.equalTo(mainTitleLabel.snp.bottom).offset(self.view.frame.height/26.1)
+            make.bottom.equalToSuperview()
+            make.left.equalTo(backButton)
+            make.centerX.equalToSuperview()
+        }
     }
     
     func layoutSetting(){
@@ -34,6 +59,7 @@ class TagManagementViewController: UIViewController {
         
         self.view.addSubview(backButton)
         self.view.addSubview(mainTitleLabel)
+        self.view.addSubview(tagAddButton)
         
         backButton.addTarget(self, action: #selector(backButtonClicked(sender:)), for: .touchUpInside)
         
@@ -48,6 +74,41 @@ class TagManagementViewController: UIViewController {
             make.left.equalTo(backButton)
             make.top.equalTo(backButton.snp.bottom).offset(self.view.frame.height/50)
         }
+        
+        tagAddButton.snp.makeConstraints { make in
+            make.centerY.equalTo(backButton)
+            make.right.equalToSuperview().offset(-self.view.frame.width/12)
+            make.height.width.equalTo(backButton)
+        }
     }
+    
+    
+    @objc func backButtonClicked(sender:UIButton){
+        self.navigationController?.popViewController(animated: true)
+    }
+    
 
 }
+
+extension TagManagementViewController: UITableViewDelegate{
+    
+}
+
+extension TagManagementViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TagTableViewCell.reuseId, for: indexPath) as! TagTableViewCell
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return self.view.frame.height/15
+    }
+    
+}
+
