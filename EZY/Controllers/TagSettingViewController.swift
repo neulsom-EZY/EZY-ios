fc016c160b52d7081cc6ffcf3b20059e5a33c360
Y00ujin <yujingim43@gmail.com>
2021-07-22 16:52:20 +0900

    [CREATE] tagNameTitleLabel, tagNameTextField, tagNameTextFieldUnderLineView created

--- EZY/Controllers/TagSettingViewController.swift
+++ EZY/Controllers/TagSettingViewController.swift
@@ -22,6 +22,23 @@ class TagSettingViewController: UIViewController {
     lazy var tagDeleteButton = UIButton().then {
         $0.setImage(UIImage(named: "EZY_DeleteButton"), for: .normal)
     }
+    
+    lazy var tagNameTitleLabel = UILabel().then {
+        $0.text = "태그 이름"
+        $0.textColor = UIColor(red: 181/255, green: 181/255, blue: 181/255, alpha: 1)
+        $0.dynamicFont(fontSize: 10, currentFontName: "AppleSDGothicNeo-SemiBold")
+    }
+    
+    lazy var tagNameTextField = UITextField().then {
+        $0.placeholder = "태그 이름을 입력해주세요"
+        $0.text = "APP PROGRAMMING"
+        $0.textColor = UIColor(red: 132/255, green: 132/255, blue: 132/255, alpha: 1)
+        $0.dynamicFont(fontSize: 13, currentFontName: "AppleSDGothicNeo-Medium")
+    }
+    
+    lazy var tagNameTextFieldUnderLineView = UIView().then{
+        $0.backgroundColor = UIColor(red: 186/255, green: 200/255, blue: 255/255, alpha: 1)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +53,9 @@ class TagSettingViewController: UIViewController {
         self.view.addSubview(backButton)
         self.view.addSubview(mainTitleLabel)
         self.view.addSubview(tagDeleteButton)
+        self.view.addSubview(tagNameTitleLabel)
+        self.view.addSubview(tagNameTextField)
+        self.view.addSubview(tagNameTextFieldUnderLineView)
         
         backButton.addTarget(self, action: #selector(backButtonClicked(sender:)), for: .touchUpInside)
         
@@ -55,6 +75,25 @@ class TagSettingViewController: UIViewController {
             make.centerY.equalTo(backButton)
             make.right.equalToSuperview().offset(-self.view.frame.width/12)
             make.height.width.equalTo(backButton)
+        }
+        
+        tagNameTitleLabel.snp.makeConstraints { make in
+            make.left.equalTo(mainTitleLabel)
+            make.top.equalTo(mainTitleLabel.snp.bottom).offset(self.view.frame.height/24.6)
+        }
+        
+        tagNameTextField.snp.makeConstraints { make in
+            make.left.equalTo(tagNameTitleLabel).offset(self.view.frame.width/30)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(tagNameTitleLabel.snp.bottom)
+            make.height.equalToSuperview().dividedBy(25.3)
+        }
+        
+        tagNameTextFieldUnderLineView.snp.makeConstraints { make in
+            make.left.equalTo(tagNameTitleLabel)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(2)
+            make.top.equalTo(tagNameTextField.snp.bottom)
         }
     }
     
