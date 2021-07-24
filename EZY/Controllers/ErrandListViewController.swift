d71f381394fe65b2dd983e5f677d07fbe22ffede
YeonJu <yjnoh1016@naver.com>
2021-07-24 13:33:29 +0900

    [UPDATE] scheduleTableView Created

--- EZY/Controllers/ErrandListViewController.swift
+++ EZY/Controllers/ErrandListViewController.swift
@@ -11,6 +11,7 @@ import Then
 
 class ErrandListViewController: UIViewController {
     //MARK: - Properties
+
     lazy var backButton = UIButton().then {
         $0.setImage(UIImage(named: "EZY_ListBackBtn"), for: .normal)
     }
@@ -25,9 +26,22 @@ class ErrandListViewController: UIViewController {
         $0.backgroundColor = .EZY_D0D0D0
     }
     
+    private lazy var scheduleTableView = ScheduleTimeTableView.init(frame: self.view.frame)
+    
+    var descriptionArray: [String] = ["부탁받은 심부름", "부탁받은 심부름", "부탁받은 심부름", "부탁받은 심부름", "부탁받은 심부름", "부탁받은 심부름", "부탁받은 심부름", "부탁받은 심부름"]
+    
+    let titleArray: [String] = ["EZY 회의", "EZY 회의", "EZY 회의", "EZY 회의", "EZY 회의", "EZY 회의", "EZY 회의", "EZY 회의"]
+    
+    let planTimeArray: [String] = ["7월 24일 AM 10:24까지", "12:00 - 13:00", "12:00 - 13:00", "12:00 - 13:00", "12:00 - 13:00", "12:00 - 13:00", "12:00 - 13:00", "12:00 - 13:00"]
+    
+    lazy var planBackgroundColor: [UIColor] = [.EZY_PLAN_FINISH_PURPLE, .EZY_PLAN_FINISH_PURPLE, .EZY_PLAN_FINISH_PURPLE, .EZY_PLAN_FINISH_PURPLE, .EZY_PLAN_FINISH_PURPLE, .EZY_PLAN_FINISH_PURPLE, .EZY_PLAN_FINISH_PURPLE, .EZY_PLAN_FINISH_PURPLE]
+    
+    lazy var planDoOrFinishColor: [UIColor] = [.EZY_PLAN_FINISH_BACK, .EZY_PLAN_FINISH_BACK, .EZY_PLAN_FINISH_BACK, .EZY_PLAN_FINISH_BACK, .EZY_PLAN_FINISH_BACK, .EZY_PLAN_FINISH_BACK, .EZY_PLAN_FINISH_BACK, .EZY_PLAN_FINISH_BACK]
+    
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        ScheduleTimeTableViewSetting()
         configureUI()
     }
     
@@ -37,7 +51,6 @@ class ErrandListViewController: UIViewController {
     func configureUI(){
         view.backgroundColor = .white
         addView()
-        cornerRadius()
         location()
     }
     
@@ -45,10 +58,7 @@ class ErrandListViewController: UIViewController {
         view.addSubview(backButton)
         view.addSubview(listName)
         view.addSubview(line)
-    }
-    
-    func cornerRadius(){
-        
+        view.addSubview(scheduleTableView)
     }
     
     func location(){
@@ -67,7 +77,79 @@ class ErrandListViewController: UIViewController {
             make.centerX.equalToSuperview()
             make.top.equalTo(listName).offset(self.view.frame.height/16.57)
         }
+        
+        scheduleTableView.snp.makeConstraints { make in
+            scheduleTableView.backgroundColor = .clear
+            make.top.equalTo(line)
+            make.centerX.equalToSuperview()
+            make.width.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+        
+        scheduleTableView.tableView.snp.makeConstraints { make in
+            make.top.equalTo(scheduleTableView).offset(self.view.frame.height/36)
+            make.width.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.centerX.equalToSuperview()
+        }
     }
+    
+    func ScheduleTimeTableViewSetting(){
+        scheduleTableView.tableView.dataSource = self
+        scheduleTableView.tableView.delegate = self
+        
+        scheduleTableView.tableView.register(ScheduleTimeTableViewCell.self, forCellReuseIdentifier: ScheduleTimeTableViewCell.ScheduleTimeTableViewIdentifier)
+    }
+
+}
+
+extension ErrandListViewController: UITableViewDataSource{
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return descriptionArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ScheduleTimeTableViewCell.ScheduleTimeTableViewIdentifier, for: indexPath) as! ScheduleTimeTableViewCell
+        
+        cell.groupNameLabel.text = descriptionArray[indexPath.row]
+        cell.titleLabel.text = titleArray[indexPath.row]
+        cell.planTimeLabel.text = planTimeArray[indexPath.row]
+        cell.EZYLISTCellLeftDecorationView.backgroundColor = planBackgroundColor[indexPath.row]
+        cell.EZYLISTCellBackground.backgroundColor = planDoOrFinishColor[indexPath.row]
+        cell.EZYLISTCellRightDecorationView.backgroundColor = planDoOrFinishColor[indexPath.row]
+        cell.titleLabel.textColor = planBackgroundColor[indexPath.row]
+        cell.groupNameLabel.textColor = planBackgroundColor[indexPath.row]
+        
+        print("tableView - cellForRowAt")
+        print(indexPath.row)
+        print(cell.titleLabel.text ?? "")
+        
+        cell.backgroundColor = .clear
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return self.view.frame.height/8
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+           return 0
+    }
+
+}
+
+extension ErrandListViewController: UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("tableView - didSelectRowAt")
+        var cell = tableView.cellForRow(at: indexPath)
+        print(indexPath.row)
+        print(titleArray[indexPath.row])
+    }
+
     
 }
 
