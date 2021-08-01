dcd6d089d42562a72e9843b3972c09f5ab0ce927
Jihoon <68891494+JiHoonAHN@users.noreply.github.com>
2021-08-01 18:19:35 +0900

    [CREATE] PrePare

--- EZY/Views/CustomData.swift
+++ EZY/Views/CustomData.swift
@@ -26,7 +26,9 @@ class CustomData : UIView{
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
-    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
