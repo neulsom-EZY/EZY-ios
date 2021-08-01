0bb38905ddae3e71313f3ed20ca885bacc1afe39
Y00ujin <yujingim43@gmail.com>
2021-08-01 19:21:57 +0900

    [UPDATE] self added

--- EZY/Controllers/PushNotificationManagementViewController.swift
+++ EZY/Controllers/PushNotificationManagementViewController.swift
@@ -51,7 +51,7 @@ class PushNotificationManagementViewController : UIViewController{
     
     //MARK: - Selectors
     @objc func backbtn(){
-        navigationController?.popViewController(animated: true)
+        self.navigationController?.popViewController(animated: true)
     }
     
     @objc func personalCalendarMove(){
