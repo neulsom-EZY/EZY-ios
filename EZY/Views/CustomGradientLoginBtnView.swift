37125c6c07b4bc0c55a84921ac9ac5c1f3fac915
YeonJu <yjnoh1016@naver.com>
2021-08-04 21:53:23 +0900

    [UPDATE] LoginButton BackImage and alpha

--- EZY/Views/CustomGradientLoginBtnView.swift
+++ EZY/Views/CustomGradientLoginBtnView.swift
@@ -19,9 +19,8 @@ class CustomGradientLoginBtnView : UIButton {
     override init(frame:CGRect) {
         super.init(frame: frame)
         setTitle("로그인", for: .normal)
-        setBackgroundImage(UIImage(named: "EZY_LoginButtonBack"), for: .normal)
+        setBackgroundImage(UIImage(named: "EZY_LoginButtonBackImg"), for: .normal)
         setTitleColor(UIColor.EZY_FFFFFF, for: .normal)
-        alpha = 0.5
     }
     
     required init?(coder: NSCoder) {
