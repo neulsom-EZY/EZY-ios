01ca24b90958c3f7e48803147167f66adf6bdf97
YeonJu <yjnoh1016@naver.com>
2021-08-08 21:56:15 +0900

    [UPDATE] NewPasswordPutVC add touchesBegan

--- EZY/Controllers/NewPasswordPutViewController.swift
+++ EZY/Controllers/NewPasswordPutViewController.swift
@@ -152,6 +152,10 @@ class NewPasswordPutViewController: UIViewController{
     func keyboardWillHide(_ sender: Notification) {
         continueButton.frame.origin.y = self.view.frame.height - continueButton.frame.height - self.view.frame.height/32.48
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        passwordField.resignFirstResponder()
+    }
 }
 
 
