0bf91e5cb2ccef9d48152737cc5896fec2ccc09d
Jihoon <68891494+JiHoonAHN@users.noreply.github.com>
2021-08-10 00:46:56 +0900

    [UPDATE] file Error

--- EZY/Controllers/ErrandCalendarViewController.swift
+++ EZY/Controllers/ErrandCalendarViewController.swift
@@ -12,7 +12,7 @@ import Then
 class ErrandCalendarViewController : UIViewController{
     //MARK: - Properties
     var manageData : [ManageData] = []
-    let titleNotification = ["심부름 완료 알림","심부름 수락 알림","심부름 거절 알림","심부름 받기 알림"]
+    let titleNotification = ["심부름 완료 알림","심부름 수락 알림","심부름 거절 알림","심부름 포기 알림","심부름 받기 알림","심부름 취소 알림","종료 심부름 미리 알림"]
     let explanation = ["상대방이 심부름을 완료했을 시 알림을 보내드립니다","상대방이 심부름 일정을 수락했을 시 알림을 보내드립니다","보낸 심부름이 거절당했을 시 알림을 보내드립니다","심부름이 왔을 때 알림을 보내드립니다"]
     
     private let backbutton = UIButton().then{
