1023b24999d0d40c68cf4e907c03cdf74dd1d772
Y00ujin <yujingim43@gmail.com>
2021-08-13 10:15:44 +0900

    [CREATE] noRepeat created

--- EZY/Resources/SceneDelegate.swift
+++ EZY/Resources/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
 
-        let mainViewController = PersonalPlanDetailViewController()
+        let mainViewController = ShowPlanViewController()
         let navigationController = UINavigationController(rootViewController: mainViewController)
 
         navigationController.navigationBar.isHidden = true
