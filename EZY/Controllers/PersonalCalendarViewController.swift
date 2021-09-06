f3a951c1115e3a6b28d509c9881fff694f707fe3
Y00ujin <yujingim43@gmail.com>
2021-09-06 17:20:49 +0900

    [UPDATE] ErrandCalendarViewController, PersonalCalendarViewController, TeamCalendarViewController extension location change

--- EZY/Controllers/PersonalCalendarViewController.swift
+++ EZY/Controllers/PersonalCalendarViewController.swift
@@ -90,4 +90,15 @@ class PersonalCalendarViewController : UIViewController{
     }
 }
 
-
+//MARK: - PersonalCalendarViewController TableView
+extension PersonalCalendarViewController : UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return manageData.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CustomManagePushNotificationCell.identifier, for: indexPath) as! CustomManagePushNotificationCell
+        cell.title.text = titleNotification[indexPath.row]
+        cell.explanation.text = explanation[indexPath.row]
+        return cell
+    }
+}
