2a382103ebda768d82e1a0bea440a7e2cd8e6af8
Y00ujin <yujingim43@gmail.com>
2021-09-27 13:49:54 +0900

    [CREATE] SelectCalendarModalView layout setting completed

--- EZY/Views/ModalViews/SelectCalendarModalView.swift
+++ EZY/Views/ModalViews/SelectCalendarModalView.swift
@@ -45,18 +45,29 @@ class SelectCalendarModalView: UIView {
     
     private let calendarAddButton = UIButton().then{
         $0.backgroundColor = UIColor(red: 170/255, green: 187/255, blue: 254/255, alpha: 1)
-        $0.setTitle("완료", for: .normal)
+        $0.setTitle("완 료", for: .normal)
         $0.dynamicFont(fontSize: 12, currentFontName: "AppleSDGothicNeo-Bold")
         $0.layer.cornerRadius = 10
     }
     
     private let dayPickerView = UIPickerView().then{
         $0.tintColor = UIColor(red: 150/255, green: 141/255, blue: 255/255, alpha: 1)
+        $0.backgroundColor = .lightGray
+    }
+    
+    private let repeatCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout()).then{
+        let layout = UICollectionViewFlowLayout()
+        $0.collectionViewLayout = layout
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.register(WhoShouldIAskCell.self, forCellWithReuseIdentifier: WhoShouldIAskCell.identifier)
+        $0.showsHorizontalScrollIndicator = false
+        $0.isScrollEnabled = false
+        $0.backgroundColor = .lightGray
     }
     
     private let repeatLabel = UILabel().then {
         $0.text = "반복 설정"
-        $0.textColor = UIColor(red: 129/255, green: 129/255, blue: 129/255, alpha: 1)
+        $0.textColor = UIColor(red: 69/255, green: 69/255, blue: 69/255, alpha: 1)
         $0.dynamicFont(fontSize: 10, currentFontName: "AppleSDGothicNeo-Thin")
     }
     
@@ -95,6 +106,12 @@ class SelectCalendarModalView: UIView {
         modalBackgroundView.addSubview(titleLabel)
         modalBackgroundView.addSubview(monthLabel)
         modalBackgroundView.addSubview(monthYearLabel)
+        modalBackgroundView.addSubview(calendarTriangleImageView)
+        modalBackgroundView.addSubview(dayPickerView)
+        modalBackgroundView.addSubview(divideLineView)
+        modalBackgroundView.addSubview(repeatLabel)
+        modalBackgroundView.addSubview(repeatCollectionView)
+        modalBackgroundView.addSubview(calendarAddButton)
     }
     
     //MARK: - addLayout
@@ -116,12 +133,52 @@ class SelectCalendarModalView: UIView {
         
         monthLabel.snp.makeConstraints { make in
             make.left.equalTo(titleLabel)
-            make.top.equalTo(titleLabel.snp.bottom).offset(viewBounds.height/33.83)
+            make.top.equalTo(titleLabel.snp.bottom).offset(viewBounds.height/45.83)
         }
         
         monthYearLabel.snp.makeConstraints { make in
             make.left.equalTo(monthLabel.snp.right).offset(viewBounds.width/75)
             make.bottom.equalTo(monthLabel).offset(-viewBounds.height/140)
+        }
+        
+        calendarTriangleImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(monthLabel.snp.bottom).offset(viewBounds.height/120)
+            make.width.equalToSuperview().dividedBy(36.66)
+            make.height.equalToSuperview().dividedBy(26.46)
+        }
+        
+        dayPickerView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.left.equalTo(monthLabel)
+            make.height.equalToSuperview().dividedBy(5.93)
+            make.top.equalTo(calendarTriangleImageView.snp.bottom).offset(viewBounds.height/116)
+        }
+        
+        divideLineView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.left.equalTo(dayPickerView)
+            make.height.equalTo(0.5)
+            make.top.equalTo(dayPickerView.snp.bottom).offset(viewBounds.height/47.76)
+        }
+        
+        repeatLabel.snp.makeConstraints { make in
+            make.left.equalTo(divideLineView)
+            make.top.equalTo(divideLineView.snp.bottom).offset(viewBounds.height/47.76)
+        }
+        
+        repeatCollectionView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.left.equalTo(repeatLabel)
+            make.top.equalTo(repeatLabel.snp.bottom).offset(viewBounds.height/73.81)
+            make.height.equalToSuperview().dividedBy(8.6)
+        }
+        
+        calendarAddButton.snp.makeConstraints { make in
+            make.right.equalTo(repeatCollectionView)
+            make.top.equalTo(repeatCollectionView.snp.bottom).offset(viewBounds.height/58)
+            make.width.equalToSuperview().dividedBy(4.71)
+            make.height.equalToSuperview().dividedBy(10.42)
         }
     }
     
