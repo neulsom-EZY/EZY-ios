79d98cfabefcc1735ab3eb9654cad307e9f74de5
Y00ujin <yujingim43@gmail.com>
2021-10-06 22:48:58 +0900

    [CREATE] 변수 접근제어 추가

--- EZY/Resources/SceneDelegate.swift
+++ EZY/Resources/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
 
-        let mainViewController = LoginViewController()
+        let mainViewController = NotificationViewController()
 
         let navigationController = UINavigationController(rootViewController: mainViewController)
 
