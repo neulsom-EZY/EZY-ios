bb22db6e7c8def95a07ff1cdb3f00486bd66e4a3
YeonJu <yjnoh1016@naver.com>
2021-10-06 10:03:33 +0900

    [UPDATE] Fix Button Layout

--- EZY/Controllers/SignUpNicknameViewController.swift
+++ EZY/Controllers/SignUpNicknameViewController.swift
@@ -117,12 +117,6 @@ class SignUpNicknameViewController: UIViewController{
             make.height.equalTo(self.view.frame.height/16.24)
         }
         
-        continueButton.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(266)
-            make.centerX.equalToSuperview()
-            make.width.equalToSuperview().dividedBy(1.23)
-            make.height.equalTo(149)
-        }
     }
     
     //MARK: - topBarViewSetting
