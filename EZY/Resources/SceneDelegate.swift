155db9ad511a7081af36a8cf50e19ee1f0e3e679
JiHoonAhn <ahnjh1028@naver.com>
2021-10-10 04:19:39 +0900

    [error]

--- EZY/Resources/SceneDelegate.swift
+++ EZY/Resources/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
 
-        let mainViewController = ShowPlanViewController()
+        let mainViewController = AddMyToDoViewController()
 
         let navigationController = UINavigationController(rootViewController: mainViewController)
 
