b3f9a832a0161979d2d8e85be5911b0882579e16
노연주 <yjnoh1016@naver.com>
2021-11-08 17:02:35 +0900

    [UPDATE] Change MainVC

--- EZY/Resources/SceneDelegate.swift
+++ EZY/Resources/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
       
-        let mainViewController = ChangePhoneNumPutViewController()
+        let mainViewController = LoginViewController()
 
 
         let navigationController = UINavigationController(rootViewController: mainViewController)
